d6799f33fca079a13f1b7ae449fc4d456eabedd7
Wolf Posdorfer <9posdorf@informatik.uni-hamburg.de>
2014-01-08 17:25:25 +0100

    init project

---
+++ mtm/src/de/inf/mobis/crawl/HTTPDownload.java
@@ -0,0 +1,43 @@
+package de.inf.mobis.crawl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class HTTPDownload
+{
+
+    public static String downloadFrom(String stringurl)
+    {
+        try
+        {
+            URL url = new URL(stringurl);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+
+            StringBuffer buffer = new StringBuffer();
+            while ((line = rd.readLine()) != null)
+            {
+                buffer.append(line);
+            }
+            rd.close();
+
+
+            return buffer.toString();
+        }
+        catch (MalformedURLException e)
+        {
+        }
+        catch (IOException e)
+        {
+        }
+        return "";
+    }
+
+}
---
+++ mtm/src/de/inf/mobis/crawl/Startup.java
@@ -0,0 +1,11 @@
+package de.inf.mobis.crawl;
+
+public class Startup
+{
+    public static void main(String[] args)
+    {
+
+        HTTPDownload.downloadFrom("http://www.google.de");
+        
+    }
+}
